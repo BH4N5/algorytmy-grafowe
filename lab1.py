8212d2b277f3e3ba480497ef78b1dfcbcfbc8184
barto <bartosz.hanc02@gmail.com>
2022-10-06 17:54:00 +0200

    Added auto-testing

--- lab1.py
+++ lab1.py
@@ -1,17 +1,17 @@
+import os, time
 from dimacs import *
-import time
-
-# method 1
-
+    
 class Node:
     def __init__(self, val):
         self.val = val
         self.parent = self
         self.rank = 0
+        
 def find( x ):
     if x.parent!=x:
         x.parent = find( x.parent )
     return x.parent
+    
 def union(x, y):
     x, y = find(x), find(y)
     if x==y: return
@@ -39,12 +39,27 @@ def Kruskal( G, V, s, t ): # G w postaci krawędziowej (u,v,weight)
         i+=1
     return min_weight
     
-start = time.time()
-V,L = loadWeightedGraph( "graphs/pp1000" )
+#-------------------------------------------------
 
-print(Kruskal(L,V,0,1))
-end = time.time()
-print(end-start)
+directory = 'graphs'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    with open( f ) as F:
+        first_line = F.readline()
+        words = first_line.split()
+    
+    V, L = loadWeightedGraph( f )
+    start = time.time()
+    ans = Kruskal( L, V, 0, 1 )
+    end = time.time()
+    T = end -start
+    print(ans)
+    print("%.4f" % T, "s")
+    
+    if ans==int(words[-1]) and T<=1 : print("OK!")
+    elif T>1: print("Za wolno!")
+    else: print("Błąd")
+    
+    print("-----------------")
 
-
-
+#-------------------------------------------------
