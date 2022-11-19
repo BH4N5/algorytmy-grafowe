21c578a41d735a926e08d0ef8cba9479429c21b7
barto <bartosz.hanc02@gmail.com>
2022-11-19 20:31:48 +0100

    Finished lab3

--- lab3/lab32.py
+++ lab3/lab32.py
@@ -1,74 +1,67 @@
 import os
 import time
-import heapq
 from dimacs import *
 
+'''
+Poniżej zaimplementowano algorytm Stoera-Wagnera dla grafu reprezentowanego
+przez macierz sąsiedztwa graph[u][v]. Funkcja mergeVertices() odpowiada za
+scalanie wierzchołków i jest łatwa do zaimplementowania w złożoności O(V) dla
+reprezentacji grafu w postaci macierzy sąsiedztwa.
 
-class Node:
-    def __init__(self) -> None:
-        self.edges = {}
-
-    def addEdge(self, to, weight):
-        self.edges[to] = self.edges.get(to, 0) + weight
-
-    def delEdge(self, to):
-        del self.edges[to]
-
-
-def convert_graph(V, L):
-    graph = [Node() for _ in range(V)]
-    for u, v, w, in L:
-        graph[u-1].addEdge(v-1, w)
-        graph[v-1].addEdge(u-1, w)
-
-    return graph
+Ze względu na wykorzystanie reprezenatcji macierzowej złożoność czasowa
+algorytmu to O(V^3). Dla grafów rzadkich jest ona gorsza od docelowej złożoności
+O(V*E + V^2 log V) uzyskiwanej poprzez wykorzystanie kolejki priorytetowej
+opartej o kopiec Fibonacciego (dla kolejki opartej o kopiec binarny mielibyśmy
+O(V*E*log V), co dla grafów gęstych jest nieefektywne). Dla grafów gęstych E ~
+V^2 złożoność obu implementacji (tj. przedstawionej i wzorcowej) jest jednakowa.
+'''
 
 
-def StoerWagner(graph):  # graph = list of nodes (type Node[])
+def StoerWagner(graph):
+    # graph = adjacency matrix; time complexity O(V^3); assuming nontrivial
+    # input i.e. connectivity(graph) > 0
 
-    def mergeVertices(graph, x, y):
-        nonlocal V
-
-        for u in graph[y].edges:
-            w = graph[y].edges[u]
-            if u != x:
-                graph[u].addEdge(x, w)
-                graph[x].addEdge(u, w)
-            graph[u].delEdge(y)
-
-        graph[y] = Node()
-        V -= 1
+    def mergeVertices(graph, s, t):  # O(V)
+        for u in range(len(graph)):
+            if u != s:
+                graph[s][u] += graph[t][u]
+                graph[u][s] += graph[t][u]
+            graph[t][u] = 0
+            graph[u][t] = 0
 
     def minimumCutPhase(graph):
         s, t = 0, 0
         processed = [False for _ in range(len(graph))]
         weightSum = [0 for _ in range(len(graph))]
 
-        heap = []
-        heapq.heappush(heap, (0, 0))
-        while len(heap) > 0:
-            u = heapq.heappop(heap)[1]
+        for _ in range(len(graph)):
+            u = weightSum.index(max(weightSum))
             if not processed[u]:
-                processed[u] = True
                 s, t = u, s
-                for v in graph[u].edges:
+                weightSum[u] = 0
+                processed[u] = True
+                for v in range(len(graph)):
                     if not processed[v]:
-                        weightSum[v] += graph[u].edges[v]
-                        heapq.heappush(heap, (-weightSum[v], v))
-
-        tmp_ans = 0
-        for u in graph[s].edges:
-            tmp_ans += graph[s].edges[u]
-
+                        weightSum[v] += graph[u][v]
+        tmp_ans = sum(graph[s])
         mergeVertices(graph, s, t)
+
         return tmp_ans
 
-    V = len(graph)
     ans = float('inf')
-    while V > 1:
+    for _ in range(len(graph)-1):
         ans = min(ans, minimumCutPhase(graph))
 
     return ans
+
+
+def convert_graph(V, L):
+    graph = [[0 for _ in range(V)] for _ in range(V)]
+    for u, v, w in L:
+        graph[u-1][v-1] = w
+        graph[v-1][u-1] = w
+
+    return graph
 
 
 def test():  # Testy
@@ -87,11 +80,13 @@ def test():  # Testy
             first_line = F.readline()
             words = first_line.split()
         V, L = loadWeightedGraph(f)
+        graph = convert_graph(V, L)
 
         start = time.time()
-        ans = StoerWagner(convert_graph(V, L))
+        ans = StoerWagner(graph)
         end = time.time()
         T = end - start
+
         print(f)
         print("Oczekiwany wynik:", int(words[-1]))
 
