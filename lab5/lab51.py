214b2c53ff9981094c990fdb1cf028cfcb64b28c
barto <bartosz.hanc02@gmail.com>
2022-12-15 13:17:49 +0100

    Added TODO to project1

---
+++ lab5/lab51.py
@@ -0,0 +1,211 @@
+import os
+import time
+from dimacs import *
+
+
+def convertGraph(V, L):
+    Graph = [set() for _ in range(V)]
+    for u, v, w in L:
+        Graph[u - 1].add(v - 1)
+        Graph[v - 1].add(u - 1)
+    return Graph
+
+
+def LexBFS(Graph):
+    sets = [{u for u in range(len(Graph))}]
+    sequence = []
+
+    while len(sets) > 0:
+        u = sets[-1].pop()
+        T = []
+        for X in sets:
+            Y, K = X & Graph[u], X - Graph[u]
+            T.append(K) if len(K) > 0 else None  # O(1)
+            T.append(Y) if len(Y) > 0 else None  # O(1)
+        sets = T
+        sequence.append(u)
+
+    return sequence
+
+
+def checkPEO(Graph, sequence):
+    RN = [set() for _ in range(len(Graph))]
+    parent = [None for _ in range(len(Graph))]
+    for u in range(len(Graph)):
+        last = None
+        for v in sequence:
+            if u == v:
+                break
+            elif v in Graph[u]:
+                RN[u].add(v)
+                last = v
+            else:
+                continue
+        parent[u] = last
+    for u in range(len(Graph)):
+        if parent[u] != None and not (RN[u] - {parent[u]} <= RN[parent[u]]):
+            return False
+
+    return True
+
+
+# Zadanie 1
+
+
+def isChordal(Graph):
+    return checkPEO(Graph, LexBFS(Graph))
+
+
+# Zadanie 2
+
+
+def maxClique(Graph):
+    sequence = LexBFS(Graph)
+    RN = [set() for _ in range(len(Graph))]
+    for u in range(len(Graph)):
+        for v in sequence:
+            if u == v:
+                break
+            elif v in Graph[u]:
+                RN[u].add(v)
+            else:
+                continue
+    K = 0
+    for u in range(len(Graph)):
+        K = max(K, len(RN[u]) + 1)
+
+    return K
+
+
+# Zadanie 3
+
+
+def chromaticNumber(Graph):
+    sequence = LexBFS(Graph)
+    color = [0 for _ in range(len(Graph))]
+    maxColor = 0
+    for u in sequence:
+        used = {color[v] for v in Graph[u]}
+        S = set(color) - used - {0}
+        if len(S) > 0:
+            c = min(list(S))
+        else:
+            maxColor += 1
+            c = maxColor
+        color[u] = c
+
+    return max(color)
+
+
+# Zadanie 4
+
+
+def minCover(Graph):
+    sequence = LexBFS(Graph)[::-1]
+    I = set()
+
+    for u in sequence:
+        if len(I & Graph[u]) == 0:
+            I.add(u)
+
+    return len(Graph) - len(I)
+
+
+'''
+def checkLexBFS(G, vs):
+    n = len(G)
+    pi = [None] * n
+    for i, v in enumerate(vs):
+        pi[v] = i
+
+    for i in range(n-1):
+        for j in range(i+1, n-1):
+            Ni = G[vs[i]]
+            Nj = G[vs[j]]
+
+            verts = [pi[v] for v in Nj - Ni if pi[v] < i]
+            if verts:
+                viable = [pi[v] for v in Ni - Nj]
+                if not viable or min(verts) <= min(viable):
+                    return False
+    return True
+
+
+directory = 'interval'
+NumOfCorrect = 0
+NumOfAll = 0
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    V, L = loadWeightedGraph(f)
+    G = convertGraph(V, L)
+    NumOfCorrect += 1 if checkLexBFS(G, LexBFS(G)) else None
+    NumOfAll += 1
+print(NumOfCorrect, "/", NumOfAll)
+'''
+
+
+def test():
+
+    numOfEx = int(input("Podaj numer zadania: "))
+
+    match numOfEx:
+        case 1:
+            func = isChordal
+            directory = 'chordal'
+            print("###### ZADANIE 1 ######")
+        case 2:
+            func = maxClique
+            directory = 'maxclique'
+            print("###### ZADANIE 2 ######")
+        case 3:
+            func = chromaticNumber
+            directory = 'coloring'
+            print("###### ZADANIE 3 ######")
+        case 4:
+            func = minCover
+            directory = 'vcover'
+            print("###### ZADANIE 4 ######")
+        case _:
+            print('Nieprawidłowa komenda. Dostępne komendy: 1, 2, 3, 4')
+            return None
+
+    Num_correct = 0
+    Num = 0
+    max_time = 100
+    for filename in os.listdir(directory):
+
+        f = os.path.join(directory, filename)
+
+        with open(f) as F:
+            first_line = F.readline()
+            words = first_line.split()
+        V, L = loadWeightedGraph(f)
+
+        start = time.time()
+        ans = func(convertGraph(V, L))
+        end = time.time()
+        T = end - start
+        print(f)
+        print("Oczekiwany wynik:", int(words[-1]))
+
+        if ans == int(words[-1]) and T <= max_time:
+            print("Wynik:", ans, "|", "%.2f" % T, "s", "|", "OK!")
+            Num_correct += 1
+
+        elif T > max_time:
+            print("Wynik:", ans, "|", "%.2f" % T, "s", "|", "Za wolno!")
+
+        else:
+            print("Wynik:", ans, "|", "%.2f" % T, "s", "|", "Błąd!")
+
+        Num += 1
+
+        print("-----------------")
+
+    print(Num_correct, "/", Num)
+    print("---------------------------")
+    print("###########################")
+    print("---------------------------")
+
+
+test()
--- project1/solve.py
+++ project1/solve.py
@@ -7,23 +7,24 @@ def my_solve(N, M, K, base, wages, eq_cost):  # Złożoność pesymistyczna O(N*
     ShowsAvailable = {artist: [(j, base[artist - 1][0] + eq_cost[j - 1] + wage)
                                for j, wage in wages[artist - 1]] for artist in range(1, N + 1)}
     TotalCost = 0
-    NumOfShows = 0
 
-    while NumOfShows < K:
+    for _ in range(K):
         artist = min(ShowsAvailable, key=lambda x: min(
             ShowsAvailable[x], key=lambda y: y[1])[1],)
         show, cost = min(ShowsAvailable[artist], key=lambda x: x[1])
 
         ShowsChosen[artist - 1].add(show)
-        NumOfShows += 1
         TotalCost += cost
         ShowsAvailable[artist] = []
 
         if len(ShowsChosen[artist - 1]) < len(base[artist - 1]):
             for j, wage in wages[artist - 1]:
                 if j not in ShowsChosen[artist - 1]:
-                    ShowsAvailable[artist].append((j, wage + eq_cost[j - 1] + base[artist - 1][len(
-                        ShowsChosen[artist - 1])] - base[artist - 1][len(ShowsChosen[artist - 1]) - 1], ))
+                    NumOfShow = len(ShowsChosen[artist-1])
+                    baseIncr = base[artist-1][NumOfShow] - \
+                        base[artist-1][NumOfShow-1]
+                    CostOfShow = wage + eq_cost[j-1] + baseIncr
+                    ShowsAvailable[artist].append((j, CostOfShow))
 
         if len(ShowsAvailable[artist]) == 0:
             del ShowsAvailable[artist]
