338f8930948a03ed637424990a822a8dc822afb6
barto <bartosz.hanc02@gmail.com>
2023-01-31 11:25:21 +0100

    Update

--- project2/example.py
+++ project2/example.py
@@ -13,9 +13,11 @@ def solve(N: int, channels) -> (int | None):  # O(E + N log N)
     d.sort(key=lambda x: -x)
 
     m = 0
-    while d[m] > m:
-        m += 1
-    m -= 1
+    for i in range(N):
+        if d[i] > i:
+            m = i
+        else:
+            break
 
     return m+1 if sum(d[:m+1]) == m*(m+1) + sum(d[m+1:]) else None
 
